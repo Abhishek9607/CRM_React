578457d9a6dc913cd3c40dc64f6f130ea901f0d0
Abhishek9607 <abhi7549ku@gmail.com>
2023-09-16 11:06:52 +0530

    completed status cards

--- CRM/src/Redux/Slices/TicketSlice.js
+++ CRM/src/Redux/Slices/TicketSlice.js
@@ -7,7 +7,14 @@ import axiosInstance from "../../config/axiosInstance";
 
 
 const initialState = {
-    ticketList: []
+    ticketList: [],
+    ticketDistribution: {
+        open: 0,
+        inProgress: 0,
+        resolved: 0,
+        onHold: 0,
+        cancelled: 0
+    }
 };
 
 export const getAllTicketsforTheUser = createAsyncThunk('tickets/getallTicketsforTheUser',async() => {
@@ -33,9 +40,20 @@ const ticketSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase(getAllTicketsforTheUser.fullfilled, (state, action) => {
+        builder.addCase(getAllTicketsforTheUser.fulfilled, (state, action) => {
                 if(!action?.payload?.data) return;
-                state.ticketList = action?.payload?.data?.result;       
+                state.ticketList = action?.payload?.data?.result; 
+                const tickets = action?.payload?.data?.result;
+                state.ticketDistribution = {
+                    open: 0,
+                    inProgress: 0,
+                    resolved: 0,
+                    onHold: 0,
+                    cancelled: 0
+                };  
+                tickets.forEach(ticket => {
+                    state.ticketDistribution[ticket.status] = state.ticketDistribution[ticket.status] + 1;
+                });
             });
     }
 });
--- CRM/src/components/card.jsx
+++ CRM/src/components/card.jsx
@@ -1,8 +1,9 @@
 
 
 const card = ({children, fontColor="text-white",borderColor="border-error", dividerColor="bg-gray-100", background="bg-blue-200", status = 50, titleText = "Card", quantity = 50}) => {
+
   return (
-    <div className={`border-b-8 ${borderColor}  w-64 h-44 rounded-md flex flex-col ${background} justify-start items-center py-2 mt-8`}>
+    <div className={`hover:scale-110 transition-all ease-out duration-300 border-b-8 ${borderColor}  w-64 h-44 rounded-md flex flex-col ${background} justify-start items-center py-2 mt-8`}>
            <div className="text-black text-2xl">
                {children} <span>{titleText}</span>
            </div>
@@ -11,7 +12,7 @@ const card = ({children, fontColor="text-white",borderColor="border-error", divi
             <div className={` text-7xl ${fontColor}`}  >
                 {quantity}
             </div>
-            <div className={`radial-progress ${fontColor}`} style={{"--value":status}}>{status}%</div>
+            <div className={`radial-progress ${fontColor}`} style={{"--value":status*100}}>{status*100}%</div>
            </div>
     </div>
   );
