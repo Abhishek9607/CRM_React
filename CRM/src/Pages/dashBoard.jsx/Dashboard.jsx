ba74a7e106d407de8302d2e7d0964e393e1b5a4a
Abhishek9607 <abhi7549ku@gmail.com>
2023-09-17 18:30:37 +0530

    Added download export file

--- CRM/src/Pages/dashBoard.jsx/Dashboard.jsx
+++ CRM/src/Pages/dashBoard.jsx/Dashboard.jsx
@@ -1,25 +1,82 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { AiOutlineDownload } from "react-icons/ai";
+import { usePDF } from "react-to-pdf";
 
+import useTickets from '../../hooks/useTickets';
 import HomeLayout from '../../layouts/HomeLayouts';
-import {getAllTicketsforTheUser} from '../../Redux/Slices/TicketSlice';
+
 
 const Dashboard = () => {
-  const ticketState = useSelector((state) =>state.tickets);
-  const dispatch = useDispatch();
-
-  async function loadTickets() {
-    const response = await dispatch(getAllTicketsforTheUser());
-    console.log(response);
-  }
-  useEffect(() => {
-    if(ticketState.ticketList.length ==  0) {
-      loadTickets();
-    }
-  }, []);
+  const [ticketState] =  useTickets();
+  const {toPDF, targetRef} = usePDF({filename: 'page.pdf'});
+ 
 
   return (
      <HomeLayout>
+      <div className='min-h-[90vh] flex flex-col items-center justify-center gap-2'>
+        <div className='bg-yellow-300 w-full text-black text-center text-3xl font-bold py-4 hover:bg-yellow-600 transition-all ease-in-out duration-300'>
+          Tickets Records <AiOutlineDownload className="cursor-pointer inline hover:text-white" onClick={() => toPDF()}/>
+        </div>
+
+        {/* <div className=' text-black text-center text-3xl font-bold py-4'>
+          <button className='btn-primary font-bold text-2xl w-full px-4 py-2 rounded-md text-white'>
+             Export to PDF
+          </button>
+        </div>
+         Table */}
+        <div className='flex flex-col w-full' ref={targetRef}>
+          {/* Title row */}
+          <div className='flex justify-between items-center gap-3 bg-purple-600 px-2 py-2 grid-cols-7'>
+              <div className='table-title basis-[8%] justify-start'>
+                 Ticket Id
+              </div>
+              <div className='table-title basis-[12%] '>
+                 Title
+              </div>
+              <div className='table-title basis-[20%]'>
+                 Description
+              </div>
+              <div className='table-title basis-[20%]'>
+                 Reporter
+              </div>
+              <div className='table-title basis-[5%]'>
+                 Priority
+              </div>
+              <div className='table-title basic-[22%]'>
+                 Assignee
+              </div>
+              <div className='table-title basis-[13%] justify-end mr-4'>
+                 Status
+              </div>
+          </div>
+          {ticketState && ticketState.ticketList.map(ticket => {
+            return (
+              <div key={ticket._id} className='flex justify-between items-center gap-3 bg-purple-600 px-2 py-2 grid-cols-7'>
+              <div className='table-title basis-[8%] justify-start'>
+                 {ticket._id.substring(0,5)+".."}
+              </div>
+              <div className='table-title basis-[12%]'>
+                 {ticket.title}
+              </div>
+              <div className='table-title basis-[20%]'>
+                 {ticket.description}
+              </div>
+              <div className='table-title basis-[20%]'>
+                 {ticket.assignee}
+              </div>
+              <div className='table-title basis-[5%]'>
+                 {ticket.ticketPriority}
+              </div>
+              <div className='table-title basis-[22%]'>
+                 {ticket.assignedTo}
+              </div>
+              <div className='table-title basis-[13%] justify-end mr-4'>
+                 {ticket.status}
+              </div>
+          </div>
+            );
+          })}
+        </div>
+      </div>
       
      </HomeLayout>
   );
--- CRM/src/Pages/home/Home.jsx
+++ CRM/src/Pages/home/Home.jsx
@@ -2,90 +2,81 @@ import { useEffect } from "react";
 import { BsFillPencilFill } from "react-icons/bs";
 import { MdCancel,MdOutlineDoneAll, MdPending } from 'react-icons/md';
 import { TbProgressBolt } from 'react-icons/tb';
-import { useDispatch, useSelector } from "react-redux";
 
 import Card from '../../components/card';
+import useTickets from "../../hooks/useTickets";
 import HomeLayouts from "../../layouts/HomeLayouts";
-import { getAllTicketsforTheUser } from "../../Redux/Slices/TicketSlice";
+
 
 const Home = () => { 
 
-  const authState = useSelector((state) => state.auth);
-  const ticketsState = useSelector((state) => state.tickets);
-
-  const dispatch = useDispatch();
-
-  async function loadTickets() {
-    const response = await dispatch(getAllTicketsforTheUser());
-    console.log(response);
-  }
-
-  useEffect(() => {
-    loadTickets();
-  }, [authState.token]);
+  
+  const [ticketsState] = useTickets();
   
   return (
     <HomeLayouts>
-      <div className="mt-10 flex flex-row justify-center items-center gap-5 flex-wrap">
-      <Card 
-         titleText="open"
-         status={ticketsState.ticketDistribution.open / ticketsState.ticketList.length}
-         quantity={ticketsState.ticketDistribution.open}
-         background="bg-yellow-300"
-         borderColor="border-green-300"
-         fontColor="text-black"
-         dividerColor="bg-black"
-      >
-        <BsFillPencilFill className='inline mr-2' />
-       </Card>
-
+     {ticketsState && (
+       <div className="mt-10 flex flex-row justify-center items-center gap-5 flex-wrap">
        <Card 
-         titleText="inProgress"
-         status={ticketsState.ticketDistribution.inProgress / ticketsState.ticketList.length}
-         quantity={ticketsState.ticketDistribution.inProgress}
-         background="bg-yellow-300" 
-         borderColor="border-green-300"
-         fontColor="text-black" 
-         dividerColor="bg-black"
-       >
-        <TbProgressBolt className='inline mr-2' />
-       </Card>
-       <Card 
-           titleText="resolved"
-           status={ticketsState.ticketDistribution.resolved / ticketsState.ticketList.length} 
-           quantity={ticketsState.ticketDistribution.resolved}
-           background="bg-yellow-300" 
-           borderColor="border-green-300" 
-           fontColor="text-black" 
-           dividerColor="bg-black"
-          >
-        <MdOutlineDoneAll className='inline mr-2' />
-       </Card>
-
-       <Card 
-           titleText="onHold"
-           status={ticketsState.ticketDistribution.onHold / ticketsState.ticketList.length} 
-           quantity={ticketsState.ticketDistribution.onHold}
-           background="bg-yellow-300" 
-           borderColor="border-green-300" 
-           fontColor="text-black" 
-           dividerColor="bg-black"
-          >
-        <MdPending className='inline mr-2' />
-       </Card>
-
-       <Card 
-          titleText="cancelled"
-          status={ticketsState.ticketDistribution.cancelled / ticketsState.ticketList.length}
-          quantity={ticketsState.ticketDistribution.cancelled}
-          background="bg-yellow-300" 
-          borderColor="border-green-300" 
+          titleText="open"
+          status={ticketsState.ticketDistribution.open / ticketsState.ticketList.length}
+          quantity={ticketsState.ticketDistribution.open}
+          background="bg-yellow-300"
+          borderColor="border-green-300"
           fontColor="text-black"
           dividerColor="bg-black"
+       >
+         <BsFillPencilFill className='inline mr-2' />
+        </Card>
+ 
+        <Card 
+          titleText="inProgress"
+          status={ticketsState.ticketDistribution.inProgress / ticketsState.ticketList.length}
+          quantity={ticketsState.ticketDistribution.inProgress}
+          background="bg-yellow-300" 
+          borderColor="border-green-300"
+          fontColor="text-black" 
+          dividerColor="bg-black"
         >
-        <MdCancel className='inline mr-2' />
-       </Card>
-      </div>
+         <TbProgressBolt className='inline mr-2' />
+        </Card>
+        <Card 
+            titleText="resolved"
+            status={ticketsState.ticketDistribution.resolved / ticketsState.ticketList.length} 
+            quantity={ticketsState.ticketDistribution.resolved}
+            background="bg-yellow-300" 
+            borderColor="border-green-300" 
+            fontColor="text-black" 
+            dividerColor="bg-black"
+           >
+         <MdOutlineDoneAll className='inline mr-2' />
+        </Card>
+ 
+        <Card 
+            titleText="onHold"
+            status={ticketsState.ticketDistribution.onHold / ticketsState.ticketList.length} 
+            quantity={ticketsState.ticketDistribution.onHold}
+            background="bg-yellow-300" 
+            borderColor="border-green-300" 
+            fontColor="text-black" 
+            dividerColor="bg-black"
+           >
+         <MdPending className='inline mr-2' />
+        </Card>
+ 
+        <Card 
+           titleText="cancelled"
+           status={ticketsState.ticketDistribution.cancelled / ticketsState.ticketList.length}
+           quantity={ticketsState.ticketDistribution.cancelled}
+           background="bg-yellow-300" 
+           borderColor="border-green-300" 
+           fontColor="text-black"
+           dividerColor="bg-black"
+         >
+         <MdCancel className='inline mr-2' />
+        </Card>
+       </div>
+     )}
 
     </HomeLayouts>
   );
